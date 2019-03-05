7b06567e74ca6a670e6d2a91d6115e071ecbbe43
pasca <razvan_baws@yahoo.com>
2019-03-05 13:19:54 +0200

    Implemented signal similarity for each pair of signals. Not sure if works :))

--- compute_signal_similarities.py
+++ compute_signal_similarities.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import numpy as np
 
 from LFP_Dataset import LFPDataset
@@ -9,7 +11,18 @@ def rmse(a, b):
     return np.sqrt(np.mean((a - b) ** 2))
 
 
-for movie in dataset.lfp_data.values():
-    for trial in range(dataset.trials_per_condition):
-        for channel in range(dataset.nr_channels - 1):
-            sim = rmse(dataset.get_dataset_piece(movie, trial, channel))
+movies = range(1, 4)
+trials = range(dataset.trials_per_condition)
+channels = range(dataset.nr_channels)
+
+combinations = list(product(*[movies, trials, channels]))
+
+for movie1, trial1, channel1 in combinations[:-1]:
+    for movie2, trial2, channel2 in combinations[1:]:
+        signal1 = dataset.get_dataset_piece(movie1, trial1, channel1)
+        signal2 = dataset.get_dataset_piece(movie2, trial2, channel2)
+        sim = rmse(signal1, signal2)
+        with open("similarities.txt", "w+") as file:
+            file.write("Similarity between {} and {}:{}".format(str(movie1) + "_" + str(trial1) + "_" + str(channel1),
+                                                                str(movie2) + "_" + str(trial2) + "_" + str(channel2),
+                                                                sim))
